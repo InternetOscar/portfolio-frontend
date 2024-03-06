bd21b5d6a1a3718dc85ec7eb8522a60ca7997ed6
Oscar Dobson-Brown <oscardobsonbrown@outlook.com>
2024-03-07 00:40:51 +0800

    add vercel analytics to portfolio

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { getStrapiMedia, getStrapiURL } from "./utils/api-helpers";
 import { fetchAPI } from "./utils/fetch-api";
+import { Analytics } from "@vercel/analytics/react"
 
 import { i18n } from "../../../i18n-config";
 import Footer from "./components/Footer";
@@ -33,6 +34,7 @@ export default async function RootLayout({
         </main>
 
         <Footer></Footer>
+        <Analytics/>
       </body>
     </html>
   );
