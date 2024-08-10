998d94de8e1e10146009cc76cae8f931ae5402f9
Oscar Dobson-Brown <oscardobsonbrown@outlook.com>
2024-08-10 15:48:29 +0800

    update cv to readcv

--- src/app/views/blog-list.tsx
+++ src/app/views/blog-list.tsx
@@ -43,79 +43,192 @@ interface Article {
   };
 }
 
-// export default function PostList(
-//   {
-//   data: articles,
-//   children,
-// }: {
-//   data: Article[];
-//   children?: React.ReactNode;
-// }) {
-//   // basic variabels for the blog groups (overall years, formatted post content)
-//   var years = []
-//   var tidyPosts = []
-//   // process the blog content to only provide necessary props
-//   for (let p = 0; p < articles.length; p++) {
-//     var postObj = {}
-//     // console.log(articles[p].attributes.publishedAt.substring(0,4))
-//     var year = parseInt(articles[p].attributes.publishedAt.substring(0,4))
-//     years.push(year)
-//     var title = articles[p].attributes.title
-//     var link = articles[p].attributes.slug
-//     var date = String(formatDate(articles[p].attributes.publishedAt))
-//     console.log(title, link, date)
-//     postObj = { ...postObj, "title": title, "link": link, "date": date, "year": year }
-//     // console.log(postObj)
-//     tidyPosts.push(postObj)
-//     // articles.filter()
-//   }
-//   // console.log(years)
-//   let uniqueYearsSet = new Set(years)
-//   let uniqueYears = Array.from(uniqueYearsSet)
-//   console.log(uniqueYears)
-//   console.log("Tidy Posts:",tidyPosts)
-//   console.log("Example Date:", tidyPosts[0])
+export default function PostList(
+  {
+  data: articles,
+  children,
+}: {
+  data: Article[];
+  children?: React.ReactNode;
+}) {
+  // basic variabels for the blog groups (overall years, formatted post content)
+  var years = []
+  var tidyPosts = []
 
-//   // const yearGroup = tidyPosts.filter(post => parseInt(post.year) == year)
+  // process the blog content to only provide necessary props
+  // go through every post so far and get the year and put it in an array
+  for (let y = 0; y < articles.length; y++) {
+    var year = String(parseInt(articles[y].attributes.publishedAt.substring(0,4)))
+    var setYear = year
+    years.push(setYear)
+  }
 
-//   // console.log(yearGroup)
+  let uniqueYearsSet = new Set(years)
+  let uniqueYears = Array.from(uniqueYearsSet)
+  // console.log("unique years array", uniqueYears)
 
-//   return (
-//     <div className="font-body font-normal col-span-4 grid grid-cols-4 mt-8 divide-y divide-neutral-200 dark:divide-neutral-800 border-t border-neutral-200 dark:border-neutral-800">
-//       {/* <div className="col-span-4 grid grid-cols-4">
-//         {uniqueYears.map(year => (
-//           <div>
-//           <p className="text-sm p-4 m-0 dark:text-neutral-300 text-blue-600">{year}</p>
-//             <div>
+  var mainGroup = []
+  let rawYears = []
+  let yearsWithPosts = {
+    "year": "",
+    "posts": []
+  }
+  // turn the years arrray into an object we can put the posts into
+  for (let a = 0; a < uniqueYears.length; a++) {
+    // console.log("years", uniqueYears[a])
+    yearsWithPosts = {...yearsWithPosts, "year": uniqueYears[a], "posts": []}
+    console.log(yearsWithPosts)
+    mainGroup.push(yearsWithPosts)
+  }
 
-//             </div>
-//           </div>
-//         ))}
-//         <div className="col-start-2 col-span-3 divide-y">
-//         {articles.map((article) => {
-//             return (
-//                 <Link
-//                 href={`/blog/${article.attributes.slug}`}
-//                 key={article.id}
-//                 className="transition-colors decoration-neutral-300 hover:decoration-neutral-400 dark:decoration-neutral-700 dark:hover:decoration-neutral-600 flex flex-col no-underline hover:bg-neutral-100 dark:hover:bg-neutral-800 dark:border-neutral-800 sm:flex-row sm:items-center gap-1 sm:gap-4 justify-between p-4"
-//                 >
-//                   <span className="m-0 text-neutral-900 dark:text-white text-sm font-normal sm:truncate">{article.attributes.title}</span>
-//                   <span className="w-[7rem] m-0 text-neutral-500 font-light dark:text-neutral-400 text-sm sm:text-right">{formatDate(article.attributes.publishedAt)}<span className="flex md:hidden"></span> </span>
-//                 </Link>
-//             );
-//           })}
-//         </div>
-//       </div> */}
-//       <p className="text-stone-500">Content coming soon, check back in a week</p>
-//       {children && children}
-//     </div>
-//   );
-// }
 
-export default function PostList() {
-  return(
-    <>
-      <p className="text-stone-300 font-body col-span-2">Nothing here yet... check back in a while</p>
-    </>
-  )
+
+
+  // set up how the post object in the blog-list should be
+  let postObject = {
+    title: "",
+    date: "",
+    link: "",
+    year: ""
+  }
+
+  // go through every post and match it to the year it was posted (and put some data with it too)
+  // go through every post and turn it into an object with the data we're looking for (for the blog list)
+  let posts = []
+  for (let p = 0; p < articles.length; p++) {
+    const post = postObject
+    var title = articles[p].attributes.title
+    var link = articles[p].attributes.slug
+    var date = String(formatDate(articles[p].attributes.publishedAt))
+    var year = String(parseInt(articles[p].attributes.publishedAt.substring(0,4)))
+    // console.log(title, link, date)
+    postObject = { ...postObject, "title": title, "link": link, "date": date, "year": year }
+    posts.push(post)
+  }
+
+  // console.log(posts)
+
+  let yearsGone = 0;
+
+  while(yearsGone < mainGroup.length) {
+    console.log("years", yearsGone)
+    yearsGone++
+
+  }
+
+  // was working and i broke it i don't want to think about it anymore
+  // match each post to a year group somehow below
+  for (let b = 0; b < posts.length; b++) {
+    let postDate = String(parseInt(articles[b].attributes.publishedAt.substring(0,4)))
+    console.log("POST DATE:", postDate)
+    // return (
+    //   <p>{postDate}</p>
+    // )
+    for (let c = 0; c < uniqueYears.length; c++) {
+      // console.log(uniqueYears[c])
+      if (postDate === uniqueYears[c]) {
+        mainGroup[c].posts.push(posts[b])
+      } else {
+
+      }
+    }
+  }
+
+  /*} target output
+  REACHED THE TARGET OUTPUT SO HAPPY WITH IT NOW
+  TODO: Make the damn thing output the right way on the web page
+  [
+    {
+      "year": "XXXX",
+      "posts": [
+        {
+          "title": "Title 6",
+          "publishedAt": "Mar 28",
+          "link": "/article-one"
+        }, {
+          "title": "Title 5",
+          "publishedAt": "Mar 25",
+          "link": "/article-two"
+        }, {
+          "title": "Title 4",
+          "publishedAt": "Mar 20",
+          "link": "/article-three"
+        },
+      ]
+    }, {
+      "year": "XXXX",
+      "posts": [
+        {
+          "title": "Title 3",
+          "publishedAt": "Dec 28",
+          "link": "/article-one"
+        }, {
+          "title": "Title 2",
+          "publishedAt": "Dec 20",
+          "link": "/article-two"
+        }, {
+          "title": "Title 1",
+          "publishedAt": "Dec 11",
+          "link": "/article-three"
+        },
+      ]
+    },
+  ]*/
+  console.log("main group", mainGroup, "years", mainGroup.length)
+
+
+  return (
+    <div className="font-body font-normal col-span-4 grid grid-cols-4 mt-8 border-neutral-200 dark:border-neutral-800 dark:text-neutral-300">
+      {/* <div className="col-span-4 grid grid-cols-4">
+        {uniqueYears.map(year => (
+          <div
+            className="col-start-1 col-span-1 border-t"
+            key={year}
+          >
+            <p className="flex text-sm p-4 m-0 dark:text-neutral-300 text-blue-600">{year}</p>
+          </div>
+        ))}
+        <div className="row-start-1 col-start-2 col-span-3 divide-y flex-col">
+          {articles.map((article) => {
+              return (
+                  <Link
+                  href={`/blog/${article.attributes.slug}`}
+                  key={article.id}
+                  className="border-t flex transition-colors decoration-neutral-300 hover:decoration-neutral-400 dark:decoration-neutral-700 dark:hover:decoration-neutral-600 flex flex-col no-underline  sm:flex-row sm:items-center gap-1 sm:gap-4 justify-between p-4"
+                  >
+                    <span className="">{article.attributes.title}</span>
+                    <span className="w-[7rem] m-0 text-neutral-500 font-light dark:text-neutral-400 text-sm sm:text-right">{formatDate(article.attributes.publishedAt)}<span className="flex md:hidden"></span> </span>
+                  </Link>
+              );
+          })}
+        </div>
+      </div> */}
+
+      <div className="col-span-4 grid grid-cols-4">
+        {mainGroup.map(({year, posts}, i) => (
+        <div className="col-start-1 col-span-1 border-t dark:border-neutral-800" key={i}>
+          <p className="flex text-sm col-span-1 p-4 m-0 dark:text-neutral-300 text-blue-600">{year}</p>
+        </div>
+        ))}
+        {posts.map((post, j) => (
+          <Link
+          href={`/blog/${post.link}`}
+          key={j}
+          className="border-t dark:border-neutral-800 dark:hover:bg-neutral-800 col-start-2 col-span-3 flex transition-colors decoration-neutral-300 hover:decoration-neutral-400 dark:decoration-neutral-700 dark:hover:decoration-neutral-600 flex flex-col no-underline  sm:flex-row sm:items-center gap-1 sm:gap-4 justify-between p-4 transition-all"
+          >
+            <span>{post.title}</span>
+            <span className={`-[7rem] m-0 text-neutral-500 font-light dark:text-neutral-400 text-sm sm:text-right row-start`}>{post.date}</span>
+          </Link>
+          )
+        )}
+
+
+      </div>
+
+
+
+      {/* DO NOT TOUCH BELOW */}
+      {children && children}
+    </div>
+  );
 }
