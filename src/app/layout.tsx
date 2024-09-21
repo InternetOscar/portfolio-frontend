a1bb03ac1505c9d4d06384193911f55636f0ed94
Oscar Dobson-Brown <oscardobsonbrown@outlook.com>
2024-09-21 15:04:52 +0800

    remove vercel analytics to stop double tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
         </main>
 
         <Footer></Footer>
-        <Analytics/>
+        {/* <Analytics/> */}
         <SpeedInsights/>
       </body>
     </html>
