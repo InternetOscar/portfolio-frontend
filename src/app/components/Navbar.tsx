b5c4be7eaafd1e1feda326580e309fc59716d367
Oscar Dobson-Brown <oscardobsonbrown@outlook.com>
2024-11-12 18:14:47 +0800

    update navbar to be sleeker and work
    removed blog nav link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,46 +1,47 @@
 "use client";
+
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState, useEffect, useRef } from "react";
 
 const Links = [
   { title: "Oscar", href: "/" },
   { title: "Projects", href: "/projects" },
   { title: "Books", href: "/books" },
-  { title: "Blog", href: "/blog" },
 ];
 
 export default function Navbar() {
   const pathname = usePathname();
   const [activeIndex, setActiveIndex] = useState(0);
   const [highlightStyle, setHighlightStyle] = useState({});
+  const [blurAmount, setBlurAmount] = useState(0);
   const navRef = useRef(null);
 
   useEffect(() => {
-    const currentIndex = Links.findIndex(link => pathname === link.href);
+    const currentIndex = Links.findIndex((link) => pathname === link.href);
     setActiveIndex(currentIndex !== -1 ? currentIndex : 0);
   }, [pathname]);
 
   useEffect(() => {
     const updateHighlight = () => {
-      const navItem = navRef.current.children[activeIndex];
-      setHighlightStyle({
-        width: `${navItem.offsetWidth}px`,
-        transform: `translateX(${navItem.offsetLeft}px)`,
-      });
+      if (navRef.current) {
+        const navItem = navRef.current.children[activeIndex];
+        setHighlightStyle({
+          width: `${navItem.offsetWidth}px`,
+          transform: `translateX(${navItem.offsetLeft}px)`,
+        });
+      }
     };
-
     updateHighlight();
-    window.addEventListener('resize', updateHighlight);
-    return () => window.removeEventListener('resize', updateHighlight);
+    window.addEventListener("resize", updateHighlight);
+    return () => window.removeEventListener("resize", updateHighlight);
   }, [activeIndex]);
-
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 bg-transparent dark:bg-neutral-900">
+      <header className="fixed top-0 left-0 right-0 z-50 bg-transparent transition-all duration-300">
         <div className="mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center h-28">
-            <nav className="absolute md:text-neutral-400 text-neutral-500 dark:text-neutral-100 font-body font-regular flex flex-row shadow-lg p-1 rounded-lg border border-neutral-300 overflow-hidden bg-white dark:bg-neutral-800 dark:border-neutral-700 backdrop-blur-md relative">
+          <div className="flex md:justify-start justify-center items-center h-28">
+            <nav className="md:text-neutral-400 text-neutral-500 dark:text-neutral-100 font-body font-regular shadow-lg p-1 rounded-lg border border-neutral-300 overflow-hidden bg-white dark:bg-neutral-800 dark:border-neutral-700 backdrop-blur-md relative">
               <div
                 className="absolute top-1 bottom-1 bg-neutral-100 dark:bg-neutral-900 dark:border dark:border-neutral-700 rounded-md transition-all duration-300 ease-in-out"
                 style={highlightStyle}
@@ -50,8 +51,12 @@ export default function Navbar() {
                   <Link
                     href={link.href}
                     key={link.title}
-                    className={`rounded-md hover:bg-neutral-100 hover:text-neutral-600 dark:hover:bg-neutral-900 transition-all px-3 py-1 ${index > 0 ? 'ml-1' : ''} dark:border-neutral-800 dark:hover:text-neutral-200 ${
-                      index === activeIndex ? 'text-neutral-800 dark:text-neutral-100' : ''
+                    className={`rounded-md hover:bg-neutral-100 hover:text-neutral-600 dark:hover:bg-neutral-900 transition-all px-3 py-1 ${
+                      index > 0 ? "ml-1" : ""
+                    } dark:border-neutral-800 dark:hover:text-neutral-200 ${
+                      index === activeIndex
+                        ? "text-neutral-800 dark:text-neutral-100"
+                        : ""
                     }`}
                   >
                     {link.title}
@@ -59,16 +64,11 @@ export default function Navbar() {
                 ))}
               </div>
             </nav>
-            <div className="md:flex font-light font-body text-neutral-400 hidden">
-              <Link className="mx-3 hover:text-neutral-800 transition-all cursor-alias" target="_blank" href="https://threads.net/oscar.dobsonbrown">Threads</Link>
-              <Link className="mx-3 hover:text-neutral-800 transition-all cursor-alias" target="_blank" href="https://github.com/internetoscar">GitHub</Link>
-              <Link className="ml-3 hover:text-neutral-800 transition-all cursor-alias" target="_blank" href="https://read.cv/oscardb">CV</Link>
-            </div>
           </div>
         </div>
       </header>
-      {/* Placeholder div to prevent content from rendering under the navbar */}
-      <div className="h-44 bg-transparent"></div>
+      {/* Spacer */}
+      <div className="h-44 bg-transparent" aria-hidden="true" />
     </>
   );
 }
